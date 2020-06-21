51d3de943b5d620f452ab61522f2f0c963769df6
Kelvin Hey <57559711+Kelvin-Hey@users.noreply.github.com>
2020-06-21 19:31:15 -0300

    Create algoritmo031.java

---
+++ src/EntradaSaida/algoritmo031.java
@@ -0,0 +1,26 @@
+/**********************************************************
+ * Entrar com o número e a base em que se deseja calcular *
+ * o logaritmo desse número e imprimi-lo.                 *
+ **********************************************************/
+ 
+ package EntradaSaida;
+ import java.util.Scanner;
+ 
+ public class algoritmo031
+ {
+     public static void main( String[] args )
+     {
+         Scanner digite = new Scanner( System.in );
+         double num, base, logaritmo;
+         
+         System.out.print( "Entre com o logaritmando: " );
+         num = digite.nextDouble();
+         System.out.print( "Entre com a base: " );
+         base = digite.nextDouble();
+         
+         logaritmo = (Math.log(num) / Math.log(base));
+         
+         System.out.println( "O logaritmo de " + num + " na base " + base +
+                             " é " + logaritmo );
+     }
+ }
